58de49b6e55df886945c31fb657b7b375ccd36f4
oeportil <ernestoportilllo09@gmail.com>
2025-03-29 19:19:13 -0600

    migraciones

---
+++ database/migrations/2025_03_30_005551_create_notificacions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notificacions', function (Blueprint $table) {
+            $table->id();
+            $table->string('tipo');
+            $table->string('mensaje');
+            $table->date('fecha_creacion');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('evento_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notificacions');
+    }
+};
